9cd0d0eff4f93bf1d3a2733a5cd4c00553b0de6e
hunter <hunterlin95119@gmail.com>
2024-10-07 13:23:49 +0800

    alpha wrong

--- graduation/single_different_alpha.m
+++ graduation/single_different_alpha.m
@@ -1,13 +1,13 @@
 %% setting the test
 N_iter=2;
 N_Solver=4;
-alpha_row=[1 5 10 20];
+alpha_row=[1 10 100 1000];
 rate_dp_matrix          =zeros(N_iter, length(alpha_row));
 N_SAR_dp_matrix         =zeros(N_iter, length(alpha_row));
 comm_rate_dp_matrix     =zeros(N_iter, length(alpha_row));
-rate_heu_matrix         =zeros(N_iter, length(alpha_row));
-N_SAR_heu_matrix        =zeros(N_iter, length(alpha_row));
-comm_rate_heu_matrix    =zeros(N_iter, length(alpha_row));
+% rate_heu_matrix         =zeros(N_iter, length(alpha_row));
+% N_SAR_heu_matrix        =zeros(N_iter, length(alpha_row));
+% comm_rate_heu_matrix    =zeros(N_iter, length(alpha_row));
 rate_sens_matrix        =zeros(N_iter, length(alpha_row));
 N_SAR_sens_matrix       =zeros(N_iter, length(alpha_row));
 comm_rate_sens_matrix   =zeros(N_iter, length(alpha_row));
@@ -17,8 +17,8 @@ comm_rate_comm_matrix   =zeros(N_iter, length(alpha_row));
 
 rate_dp_matrix_normalized=          zeros(N_iter,length(alpha_row));
 comm_rate_dp_matrix_normalized=     zeros(N_iter,length(alpha_row));
-rate_heu_matrix_normalized=         zeros(N_iter,length(alpha_row));
-comm_rate_heu_matrix_normalized=    zeros(N_iter,length(alpha_row));
+% rate_heu_matrix_normalized=         zeros(N_iter,length(alpha_row));
+% comm_rate_heu_matrix_normalized=    zeros(N_iter,length(alpha_row));
 rate_sens_matrix_normalized=        zeros(N_iter,length(alpha_row));
 comm_rate_sens_matrix_normalized=   zeros(N_iter,length(alpha_row));
 rate_comm_matrix_normalized=        zeros(N_iter,length(alpha_row));
@@ -36,9 +36,9 @@ for ii=1:N_iter
         rate_dp_matrix(ii,jj)           =rate_dp;
         N_SAR_dp_matrix(ii,jj)          =N_SAR_dp;
         comm_rate_dp_matrix(ii,jj)      =comm_rate_dp;
-        rate_heu_matrix (ii,jj)         =rate_heu;
-        N_SAR_heu_matrix(ii,jj)         =N_SAR_heu;
-        comm_rate_heu_matrix(ii,jj)     =comm_rate_heu;
+        % rate_heu_matrix (ii,jj)         =rate_heu;
+        % N_SAR_heu_matrix(ii,jj)         =N_SAR_heu;
+        % comm_rate_heu_matrix(ii,jj)     =comm_rate_heu;
         rate_sens_matrix(ii,jj)         =rate_sens;
         N_SAR_sens_matrix(ii,jj)        =N_SAR_sens;
         comm_rate_sens_matrix(ii,jj)    =comm_rate_sens;
@@ -64,8 +64,8 @@ for ii=1:N_iter
 
         rate_dp_matrix_normalized(ii,jj)=       rate_dp_normalized;
         comm_rate_dp_matrix_normalized(ii,jj)=  comm_rate_dp_normalized;
-        rate_heu_matrix_normalized(ii,jj)=      rate_heu_normalized;
-        comm_rate_heu_matrix_normalized(ii,jj)= comm_rate_heu_normalized;
+        % rate_heu_matrix_normalized(ii,jj)=      rate_heu_normalized;
+        % comm_rate_heu_matrix_normalized(ii,jj)= comm_rate_heu_normalized;
         rate_sens_matrix_normalized(ii,jj)=     rate_sens_normalized;
         comm_rate_sens_matrix_normalized(ii,jj)=comm_rate_sens_normalized;
         rate_comm_matrix_normalized(ii,jj)=     rate_comm_normalized;
@@ -75,13 +75,18 @@ for ii=1:N_iter
 
     end
 end
+mean_rate_dp=mean(rate_dp_matrix);
+% mean_rate_heu=mean(rate_heu_matrix);
+mean_rate_sens=mean(rate_sens_matrix);
+mean_rate_comm=mean(rate_comm_matrix);
+
 mean_rate_dp_normalized=mean(rate_dp_matrix_normalized);
-mean_rate_heu_normalized=mean(rate_heu_matrix_normalized);
+% mean_rate_heu_normalized=mean(rate_heu_matrix_normalized);
 mean_rate_sens_normalized=mean(rate_sens_matrix_normalized);
 mean_rate_comm_normalized=mean(rate_comm_matrix_normalized);
 
 mean_N_SAR_dp=mean(N_SAR_dp_matrix);
-mean_N_SAR_heu=mean(N_SAR_heu_matrix);
+% mean_N_SAR_heu=mean(N_SAR_heu_matrix);
 mean_N_SAR_sens=mean(N_SAR_sens_matrix);
 mean_N_SAR_comm=mean(N_SAR_comm_matrix);
 
@@ -89,33 +94,58 @@ mean_comm_rate_dp_normalized=mean(comm_rate_dp_matrix_normalized);
 mean_comm_rate_heu_normalized=mean(comm_rate_heu_matrix_normalized);
 mean_comm_rate_sens_nomalized=mean(comm_rate_sens_matrix_normalized);
 mean_comm_rate_comm_normalized=mean(comm_rate_comm_matrix_normalized);
+%%
 
 figure()
-plot(alpha_row, mean_rate_dp_normalized);
+semilogx(alpha_row, mean_rate_dp, '-o');
 hold on
-plot(alpha_row, mean_rate_heu_normalized);
-plot(alpha_row, mean_rate_sens_normalized);
-plot(alpha_row, mean_rate_comm_normalized);
-legend('DP(Proposed)', 'Heurstic', 'Sensing only', 'Communication only')
+% semilogx(time_slot_max_row, mean_rate_heu, '-^');
+semilogx(alpha_row, mean_rate_sens, '-x');
+semilogx(alpha_row, mean_rate_comm, '-*');
+ylabel bits
+xlabel('times of average rate')
+grid on
+legend('DP(Proposed)', 'Sensing only', 'Communication only')
+title("Average performance on different alpha")
+
 
 figure()
-plot(alpha_row, mean_N_SAR_dp);
+semilogx(alpha_row, mean_rate_dp_normalized);
 hold on
-plot(alpha_row, mean_N_SAR_heu);
-plot(alpha_row, mean_N_SAR_sens);
-plot(alpha_row, mean_N_SAR_comm);
-legend('DP(Proposed)', 'Heurstic', 'Sensing only', 'Communication only')
+% semilogx(alpha_row, mean_rate_heu_normalized);
+semilogx(alpha_row, mean_rate_sens_normalized);
+semilogx(alpha_row, mean_rate_comm_normalized);
+xlabel('times of average rate')
+grid on
+legend('DP(Proposed)', 'Sensing only', 'Communication only')
+title("Average normalized performance on different alpha")
 
 figure()
-plot(alpha_row, mean_comm_rate_dp_normalized);
+semilogx(alpha_row, mean_N_SAR_dp);
 hold on
-plot(alpha_row, mean_comm_rate_heu_normalized);
-plot(alpha_row, mean_comm_rate_sens_nomalized);
-plot(alpha_row, mean_comm_rate_comm_normalized);
-legend('DP(Proposed)', 'Heurstic', 'Sensing only', 'Communication only')
+% semilogx(alpha_row, mean_N_SAR_heu);
+semilogx(alpha_row, mean_N_SAR_sens);
+semilogx(alpha_row, mean_N_SAR_comm);
+ylabel cells
+xlabel('times of average rate')
+grid on
+legend('DP(Proposed)', 'Sensing only', 'Communication only')
+title("Average SAR coverage on different alpha")
+
+figure()
+semilogx(alpha_row, mean_comm_rate_dp_normalized);
+hold on
+% semilogx(alpha_row, mean_comm_rate_heu_normalized);
+semilogx(alpha_row, mean_comm_rate_sens_nomalized);
+semilogx(alpha_row, mean_comm_rate_comm_normalized);
+ylabel bits
+xlabel('times of average rate')
+grid on
+legend('DP(Proposed)', 'Sensing only', 'Communication only')
+title("Average communication performance on different alpha")
 
 
-save('single_different_time.mat')
+save('single_different_alpha.mat')
 
 % assume cell_matrix is done
 
