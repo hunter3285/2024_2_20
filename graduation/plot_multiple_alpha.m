f556b3db99e376027d553c57c3ec4abef32a7204
hunter <hunterlin95119@gmail.com>
2024-10-09 15:37:20 +0800

    figures

--- graduation/plot_multiple_alpha.m
+++ graduation/plot_multiple_alpha.m
@@ -1,11 +1,6 @@
 % 1. load 原本的
 % 2. 改第二個section的檔名
-clear all
-load("multiple_different_time2.mat")
-%%
-mean_comm_rate_dp=mean(comm_rate_dp_matrix);
-mean_comm_rate_sens=mean(comm_rate_sens_matrix);
-mean_comm_rate_comm=mean(comm_rate_comm_matrix);
+load('multiple_different_alpha2.mat')
 %%
 
 mean_rate_dp_old=mean_rate_dp;
@@ -25,11 +20,11 @@ mean_comm_rate_dp_old=mean_comm_rate_dp;
 mean_comm_rate_sens_old=mean_comm_rate_sens;
 mean_comm_rate_comm_old=mean_comm_rate_comm;
 %%
-load('multiple_different_time.mat', 'mean_rate_dp', 'mean_rate_sens', 'mean_rate_comm')
-load('multiple_different_time.mat', 'mean_rate_dp_normalized', 'mean_rate_sens_normalized', 'mean_rate_comm_normalized')
-load('multiple_different_time.mat', 'mean_N_SAR_dp', 'mean_N_SAR_sens', 'mean_N_SAR_comm')
-load('multiple_different_time.mat', 'mean_comm_rate_dp_normalized', 'mean_comm_rate_sens_nomalized', 'mean_comm_rate_comm_normalized')
-load('multiple_different_time.mat', 'mean_comm_rate_dp', 'mean_comm_rate_sens', 'mean_comm_rate_comm')
+load('multiple_different_alpha.mat', 'mean_rate_dp', 'mean_rate_sens', 'mean_rate_comm')
+load('multiple_different_alpha.mat', 'mean_rate_dp_normalized', 'mean_rate_sens_normalized', 'mean_rate_comm_normalized')
+load('multiple_different_alpha.mat', 'mean_N_SAR_dp', 'mean_N_SAR_sens', 'mean_N_SAR_comm')
+load('multiple_different_alpha.mat', 'mean_comm_rate_dp_normalized', 'mean_comm_rate_sens_nomalized', 'mean_comm_rate_comm_normalized')
+load('multiple_different_alpha.mat', 'mean_comm_rate_dp', 'mean_comm_rate_sens', 'mean_comm_rate_comm')
 %%
 mean_rate_dp=(mean_rate_dp+mean_rate_dp_old)/2;
 mean_rate_sens=(mean_rate_sens+mean_rate_sens_old)/2;
@@ -50,62 +45,61 @@ mean_comm_rate_comm=(mean_comm_rate_comm+mean_comm_rate_comm_old)/2;
 %%
 
 figure()
-plot(time_slot_max_row, mean_rate_dp, '-o');
+plot(alpha_row, mean_rate_dp, '-o');
 hold on
 % plot(time_slot_max_row, mean_rate_heu, '-^');
-plot(time_slot_max_row, mean_rate_sens, '-x');
-plot(time_slot_max_row, mean_rate_comm, '-*');
+plot(alpha_row, mean_rate_sens, '-x');
+plot(alpha_row, mean_rate_comm, '-*');
 ylabel('Total performance (bits)')
-xlabel('Time limit (Time slots)')
+xlabel('alpha (times of average rate)')
 grid on
 legend('DP(Proposed)', 'Sensing only', 'Communication only')
-% title("Average performance on different time limit")
+% title("Average performance on different alpha")
 
 figure()
-plot(time_slot_max_row, mean_rate_dp_normalized, '-o');
+plot(alpha_row, mean_rate_dp_normalized, '-o');
 hold on
 % plot(time_slot_max_row, mean_rate_heu_normalized, '-^');
-plot(time_slot_max_row, mean_rate_sens_normalized, '-x');
-plot(time_slot_max_row, mean_rate_comm_normalized, '-*');
-xlabel('Time limit (Time slots)')
+plot(alpha_row, mean_rate_sens_normalized, '-x');
+plot(alpha_row, mean_rate_comm_normalized, '-*');
+xlabel('alpha (times of average rate)')
 ylabel('Total normalized performance')
 grid on
 legend('DP(Proposed)', 'DP for Sensing only', 'DP for Communication only')
-% title("Average normalized performance on different time limit")
+% title("Average normalized performance on different alpha")
 
 figure()
-plot(time_slot_max_row, mean_N_SAR_dp, '--o');
+plot(alpha_row, mean_N_SAR_dp, '--o');
 hold on
 % plot(time_slot_max_row, mean_N_SAR_heu, '--^');
-plot(time_slot_max_row, mean_N_SAR_sens, '--x');
-plot(time_slot_max_row, mean_N_SAR_comm, '--*');
+plot(alpha_row, mean_N_SAR_sens, '--x');
+plot(alpha_row, mean_N_SAR_comm, '--*');
 ylabel('Coverage (cells)')
-xlabel('Time limit (Time slots)')
+xlabel('alpha (times of average rate)')
 grid on
 legend('DP(Proposed)', 'DP for Sensing only', 'DP for Communication only')
 % title("Average SAR coverage on different time limit")
 
 figure()
-plot(time_slot_max_row, mean_comm_rate_dp_normalized, '-o');
+plot(alpha_row, mean_comm_rate_dp_normalized, '-o');
 hold on
 % plot(time_slot_max_row, mean_comm_rate_heu_normalized, '-^');
-plot(time_slot_max_row, mean_comm_rate_sens_nomalized, '-x');
-plot(time_slot_max_row, mean_comm_rate_comm_normalized, '-*');
-xlabel('Time limit (Time slots)')
+plot(alpha_row, mean_comm_rate_sens_nomalized, '-x');
+plot(alpha_row, mean_comm_rate_comm_normalized, '-*');
+xlabel('alpha (times of average rate)')
 ylabel('Normalized communication performance')
 grid on
 legend('DP(Proposed)', 'DP for Sensing only', 'DP for Communication only')
-% title("Average communiation performance on different time limit")
-
+% title("Average normalized communiation performance on different alpha")
 
 figure()
-plot(time_slot_max_row, mean_comm_rate_dp, '-o');
+plot(alpha_row, mean_comm_rate_dp, '-o');
 hold on
 % plot(time_slot_max_row, mean_comm_rate_heu_normalized, '-^');
-plot(time_slot_max_row, mean_comm_rate_sens, '-x');
-plot(time_slot_max_row, mean_comm_rate_comm, '-*');
-xlabel('Time limit (Time slots)')
-ylabel('Communication performance (bits)')
+plot(alpha_row, mean_comm_rate_sens, '-x');
+plot(alpha_row, mean_comm_rate_comm, '-*');
+xlabel('alpha (times of average rate)')
+ylabel('Communication performance')
 grid on
 legend('DP(Proposed)', 'DP for Sensing only', 'DP for Communication only')
-% title("Average communiation performance on different time limit")
+% title("Average communiation performance on different alpha")
